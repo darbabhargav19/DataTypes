576f373b7960cfe203bcac55089f9420db640878
Darbha Bhargav <darbabhargav19@gmail.com>
2025-10-08 19:30:50 +0530

    Add files via upload

---
+++ DataTypes.py
@@ -0,0 +1,36 @@
+bike= "Random"
+email ="random@gmail.com"
+
+#strings
+print("Hi bro this is owner")
+print(f"do you like to drive {bike}")
+print(f"book your bike contact: {email}")
+
+#integers
+staff = 25
+quantity = 50
+
+print(f"we have {staff} members working")
+print(f"we have {quantity} bikes")     
+
+#float
+milage = 45.45
+
+print(f"The milage of this bike is {milage}km")
+
+#boolean
+
+offer= False
+Good_price = True
+
+if offer:
+    print("yes it is in offer")
+else:
+    print("No it is not in offer")
+
+if Good_price:
+    print("yes it is Good price")
+else:
+    print("No it is not Good Price")
+
+
